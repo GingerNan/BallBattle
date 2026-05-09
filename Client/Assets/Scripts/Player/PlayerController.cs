872016e0f9be7a9e0dd22b0d82ed187b291ba4a8
ginger <ginger_nan@163.com>
2026-05-09 18:02:12 +0800

    update

---
+++ Client/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject ballPrefab;     //自己分裂出来的球
+}
