b9a58ba73a7c9dcb62cc33ea424f81708dc5df83
cola <ginger_nan@163.com>
2026-05-11 22:05:39 +0800

    完善接受消息框架

--- Server/Client.cs
+++ Server/Client.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace Server
 {
     public class Client
     {
-        private Socket Socket;
+        private Socket Socket {get; set;}
         public string PlayerId { get; private set; }
+        public bool IsConnected => Socket != null && Socket.Connected;
+        
+        private List<byte> _receiveBuffer = new List<byte>();   // 累计接收消息的缓冲区
+        private int _expectedBodyLength = -1;   // 期望消息长度
+        
         
         public Client(Socket socket)
         {
@@ -39,16 +46,118 @@ namespace Server
                 int bytesRead = Socket.EndReceive(ar);
                 if (bytesRead > 0)
                 {
+                    // 这一次收到的数据
                     byte[] buffer = ar.AsyncState as byte[];
                     
-                    string msg = Encoding.UTF8.GetString(buffer);
-                    Console.WriteLine(msg);
+                    byte[] newData = new byte[bytesRead];
+                    Array.Copy(buffer, newData, bytesRead);
+                    _receiveBuffer.AddRange(newData);
+
+                    // 处理累计缓冲区数据
+                    ProcessReceiveData();
+                    
+                    // 接续接受
+                    StartReceiving();
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
+            }
+        }
+
+        // 向客户端发送消息
+        public void Send(string msg)
+        {
+            try
+            {
+                byte[] bodyData = Encoding.UTF8.GetBytes(msg);
+                byte[] headerData = BitConverter.GetBytes(bodyData.Length);
+                // 合并消息头和消息体
+                byte[] totalData = new  byte[headerData.Length + bodyData.Length];
+                Buffer.BlockCopy(headerData, 0, totalData, 0, headerData.Length);
+                Buffer.BlockCopy(bodyData, 0, totalData, headerData.Length, bodyData.Length);
+                
+                Socket.BeginSend(totalData, 0, totalData.Length, SocketFlags.None, SendCallback, null);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket.EndSend(ar);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 解析消息
+        /// </summary>
+        private void ProcessReceiveData()
+        {
+            try
+            {
+                while (_receiveBuffer.Count > 4)
+                {
+                    if (_expectedBodyLength == -1)
+                    {
+                        byte[] headBytes = _receiveBuffer.GetRange(0, 4).ToArray();
+                        _expectedBodyLength = BitConverter.ToInt32(headBytes, 0);
+                    }
+
+                    // 判断是否是一条完整的消息
+                    if (_receiveBuffer.Count >=  4 + _expectedBodyLength)
+                    {
+                        // 提取消息体
+                        byte[] bodyBytes = _receiveBuffer.GetRange(4, _expectedBodyLength).ToArray();
+                        string msg = Encoding.UTF8.GetString(bodyBytes);
+                        
+                        // 实际处理消息
+                        ProcessMessage(msg);
+                        
+                        // 从缓冲区移除已处理数据
+                        _receiveBuffer.RemoveRange(0, 4 + _expectedBodyLength);
+                        _expectedBodyLength = -1;
+                    }
+                    else
+                    {
+                        // 数据不完整，等待下次接受
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 实际处理消息方法
+        /// </summary>
+        /// <param name="msg"></param>
+        private void ProcessMessage(string msg)
+        {
+            NetworkMessage networkMessage = JsonConvert.DeserializeObject<NetworkMessage>(msg);
+
+            switch (networkMessage.Type)
+            {
+                case MessageType.PlayerJoin:
+                    break;
+                case MessageType.PlayerLeave:
+                    break;
             }
         }
     }
---
+++ Server/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Server
+{
+    // 消息类型
+    public enum MessageType
+    {
+        PlayerJoin,
+        GiveThePlayerId,
+        GenerateMap,
+        SendPosition,   // 客户端发送位置和状态
+        SyncPosition,   // 服务器同步所有玩家位置
+        PlayerLeave,
+        GenerateFood,
+        RemoveFood,
+        SyncFoods,
+        PlayerVomit,
+        PlayerEat,
+    }
+    
+    [Serializable]
+    public class NetworkMessage
+    {
+        public MessageType Type { get; set; }
+    }
+}
